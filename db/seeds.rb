f5dd80d5a7f6e392001b7d49bdd29c336d065021
kirimari <ait.mus06@gmail.com>
2019-01-06 16:42:55 +0100

    add Categry seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.delete_all
+first_category = Category.create(:name => "Category 1" )
+second_category = Category.create(:name => "Category 2" )
